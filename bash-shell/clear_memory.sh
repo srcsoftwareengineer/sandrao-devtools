b40367fe4ad1cc9ac129dc54f3d5a05ae41ac84a
samurai <projetaty@gmail.com>
2021-08-03 13:01:56 -0300

    Changed the coloring vars position out of 'set' parameter

--- bash-shell/clear_memory.sh
+++ bash-shell/clear_memory.sh
@@ -12,41 +12,43 @@
 # There are also some planned improvements to be coded days ahead.
 # ===============================================================================================
 
+# Setting coloring vars
+white="\033[0m"
+yellowbold="\033[1;33m"	
+yellow="\033[0;33m"
+red="\033[1;31m"
+light_greenbold="\033[1;92m"
+blue="\033[1;36m"
+light_blue="\033[0;94m"
+
 set -e
-	white="\033[0m"
-	yellowbold="\033[1;33m"	
-	yellow="\033[0;33m"
-	red="\033[1;31m"
-	light_greenbold="\033[1;92m"
-	blue="\033[1;36m"
-	light_blue="\033[0;94m"
-	
+
 	function log_starting_tasks {
 		log_blank_line
 		echo -e $red"...::::: Run Tasks :::::..."
 	}
-	
+
 	function log_tasks_done {
 		echo -e $light_greenbold"...::::: All Tasks Are Done!"$white
 		log_blank_line
 	}
-	
+
 	function log_initial_header {
 		echo -e $yellow"|===========================================================================================|"
 		echo "| Current Memory Usage                                                                      |"
 		echo -e "|===========================================================================================|"$white
 	}
-	
+
 	function log_resulting_header {
 		echo -e $yellowbold"|===========================================================================================|"
 		echo "| Resulting Memory Usage                                                                    |"
 		echo -e "|===========================================================================================|"$white
 	}
-	
+
 	function log_blank_line {
 		echo -e ""
 	}
-	
+
 	function check_root_user {
 		root_user=$(id -u);
 		if ! [ $root_user -eq 0 ]; then
@@ -54,21 +56,21 @@ set -e
 		   exit 1;
 		fi
 	}
-	
+
 	function get_memory_usage {
 		free -hltw;
 	}
-	
+
 	function run_task1 {
 		echo -e $blue"...:: 1: "$light_blue"Dropping memory cache $light_greenbold[ok]"
 		sync && echo 4 > /proc/sys/vm/drop_caches;
 	}
-	
+
 	function run_task2 {
 		echo -e $blue"...:: 2: "$light_blue"Turning off swapp     $light_greenbold[ok]"
 		swapoff -a;
 	}
-	
+
 	function run_task3 {
 		echo -e $blue"...:: 3: "$light_blue"Turning on swapp      $light_greenbold[ok]"
 		swapon -a;
