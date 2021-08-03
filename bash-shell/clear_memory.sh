51f4a880b58ad4f5b76d6474aef44fe0aab25b3a
samurai <projetaty@gmail.com>
2021-08-03 13:51:46 -0300

    Few code rearrange

--- bash-shell/clear_memory.sh
+++ bash-shell/clear_memory.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# ===============================================================================================
+# ============================================================================================================
 # Created on 2021-08-03
 # @program: clear_memory
 # @version: 1.0
 # @author: Sandro Regis Cardoso | Software Engineer
 # @contact: src.softwareengineer@gmail.com
-# @summary: Program to free memory usage. This starting coding for this program was catched from internet and once I use it from a long, 
-# long time unfortunely I don't remember the source where I found it.
+# @summary: Program to free memory usage. This starting coding for this program was catched from internet
+# and as I use it from a long, long time, unfortunely I don't remember the source where I found it.
 # It was adapted to be executed on Linux Debian distribuition and it works from 6.0 or higher.
 # Some initial enhancements was did few days ago until today.
 # There are also some planned improvements to be coded days ahead.
-# ===============================================================================================
+# ============================================================================================================
 
 # Setting coloring vars
 white="\033[0m"
@@ -20,6 +20,7 @@ red="\033[1;31m"
 light_greenbold="\033[1;92m"
 blue="\033[1;36m"
 light_blue="\033[0;94m"
+
 
 set -e
 
@@ -52,7 +53,7 @@ set -e
 	function check_root_user {
 		root_user=$(id -u);
 		if ! [ $root_user -eq 0 ]; then
-		   echo -e $yellow"This program should be executed$red as root user!"$white
+		   echo -e $yellow"This program need$red root user privilegies!"$white
 		   exit 1;
 		fi
 	}
