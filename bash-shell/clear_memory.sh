ac64a478ef6e4fed8fb49f7569d69df002b4f37d
SANDRO REGIS CARDOSO <src.softwareengineer@gmail.com>
2024-04-11 18:25:21 -0300

    Update clear_memory.sh

--- bash-shell/clear_memory.sh
+++ bash-shell/clear_memory.sh
@@ -11,7 +11,6 @@
 # Some initial enhancements was did few days ago until today.
 # There are also some planned improvements to be coded days ahead.
 # ============================================================================================================
-
 # Setting coloring vars
 white="\033[0m"
 yellowbold="\033[1;33m"	
@@ -20,34 +19,28 @@ red="\033[1;31m"
 light_greenbold="\033[1;92m"
 blue="\033[1;36m"
 light_blue="\033[0;94m"
-
 set -e
 	function log_starting_tasks {
 		log_blank_line
 		echo -e $red"...::::: Run Tasks :::::..."
 	}
-
 	function log_tasks_done {
 		echo -e $light_greenbold"...::::: All Tasks Are Done!"$white
 		log_blank_line
 	}
-
 	function log_initial_header {
 		echo -e $yellow"|===========================================================================================|"
 		echo "| Current Memory Usage                                                                      |"
 		echo -e "|===========================================================================================|"$white
 	}
-
 	function log_resulting_header {
 		echo -e $yellowbold"|===========================================================================================|"
 		echo "| Resulting Memory Usage                                                                    |"
 		echo -e "|===========================================================================================|"$white
 	}
-
 	function log_blank_line {
 		echo -e ""
 	}
-
 	function check_root_user {
 		root_user=$(id -u);
 		if ! [ $root_user -eq 0 ]; then
@@ -55,17 +48,14 @@ set -e
 		   exit 1;
 		fi
 	}
-
 	function get_memory_usage {
 		free -hltw;
 	}
-
 	function run_task1 {
 		echo -e $blue"...:: 1: "$light_blue"Dropping shared memory $light_greenbold[ok]"
 		sync && echo 1 > /proc/sys/vm/drop_caches;
 		sleep 5;
 	}
-
 	function run_task2 {
 		echo -e $blue"...:: 2: "$light_blue"Dropping buffers memory $light_greenbold[ok]"
 		sync && echo 2 > /proc/sys/vm/drop_caches;
@@ -83,19 +73,16 @@ set -e
 		sync && echo 4 > /proc/sys/vm/drop_caches;
 		sleep 5;
 	}
-
 	function run_task5 {
 		echo -e $blue"...:: 5: "$light_blue"Turning off swapp     $light_greenbold[ok]"
 		swapoff -a;
 		sleep 5;
 	}
-
 	function run_task6 {
 		echo -e $blue"...:: 6: "$light_blue"Turning on swapp      $light_greenbold[ok]"
 		swapon -a;
 		sleep 5;
 	}
-
 if check_root_user; then
 	log_initial_header
 	get_memory_usage
