d514a21a373374d3e64b48d72bc6a140ebd4e79f
SANDRO REGIS CARDOSO <src.softwareengineer@gmail.com>
2025-06-23 00:40:23 -0300

    Update clear_memory.sh
    fixes on echo lines calling vars

--- bash-shell/clear_memory.sh
+++ bash-shell/clear_memory.sh
@@ -24,21 +24,21 @@ light_blue="\033[0;94m"
 set -e
 	function log_starting_tasks {
 		log_blank_line
-		echo -e $red"...::::: Run Tasks :::::..."
+		echo -e $red "...::::: Run Tasks :::::..."
 	}
 	function log_tasks_done {
-		echo -e $light_greenbold"...::::: All Tasks Are Done!"$white
+		echo -e $light_greenbold "...::::: All Tasks Are Done!" $white
 		log_blank_line
 	}
 	function log_initial_header {
-		echo -e $yellow"|===========================================================================================|"
+		echo -e $yellow "|===========================================================================================|"
 		echo "| Current Memory Usage                                                                      |"
-		echo -e "|===========================================================================================|"$white
+		echo -e "|===========================================================================================|" $white
 	}
 	function log_resulting_header {
-		echo -e $yellowbold"|===========================================================================================|"
+		echo -e $yellowbold "|===========================================================================================|"
 		echo "| Resulting Memory Usage                                                                    |"
-		echo -e "|===========================================================================================|"$white
+		echo -e "|===========================================================================================|" $white
 	}
 	function log_blank_line {
 		echo -e ""
@@ -46,7 +46,7 @@ set -e
 	function check_root_user {
 		root_user=$(id -u);
 		if ! [ $root_user -eq 0 ]; then
-		   echo -e $yellow"This program need$red root user privilegies!"$white
+		   echo -e $yellow"This program need$red root user privilegies!" $white
 		   exit 1;
 		fi
 	}
@@ -54,34 +54,34 @@ set -e
 		free -hltw;
 	}
 	function run_task1 {
-		echo -e $blue"...:: 1: "$light_blue"Dropping shared memory $light_greenbold[ok]"
+		echo -e $blue "...:: 1: "$light_blue"Dropping shared memory $light_greenbold[ok]"
 		sync && echo 1 > /proc/sys/vm/drop_caches;
 		sleep 5;
 	}
 	function run_task2 {
-		echo -e $blue"...:: 2: "$light_blue"Dropping buffers memory $light_greenbold[ok]"
+		echo -e $blue "...:: 2: "$light_blue"Dropping buffers memory $light_greenbold[ok]"
 		sync && echo 2 > /proc/sys/vm/drop_caches;
 		sleep 5;
 	}
 
 	function run_task3 {
-		echo -e $blue"...:: 3: "$light_blue"Running.... $light_greenbold[ok]"
+		echo -e $blue "...:: 3: "$light_blue"Running.... $light_greenbold[ok]"
 		sync && echo 3 > /proc/sys/vm/drop_caches;
 		sleep 5;
 	}
 
 	function run_task4 {
-		echo -e $blue"...:: 4: "$light_blue"Reinforcing cleanse $light_greenbold[ok]"
+		echo -e $blue "...:: 4: "$light_blue"Reinforcing cleanse $light_greenbold[ok]"
 		sync && echo 3 > /proc/sys/vm/drop_caches;
 		sleep 5;
 	}
 	function run_task5 {
-		echo -e $blue"...:: 5: "$light_blue"Turning off swapp     $light_greenbold[ok]"
+		echo -e $blue "...:: 5: "$light_blue"Turning off swapp     $light_greenbold[ok]"
 		swapoff -a;
 		sleep 5;
 	}
 	function run_task6 {
-		echo -e $blue"...:: 6: "$light_blue"Turning on swapp      $light_greenbold[ok]"
+		echo -e $blue "...:: 6: "$light_blue"Turning on swapp      $light_greenbold[ok]"
 		swapon -a;
 		sleep 5;
 	}
